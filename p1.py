76bfeaddef6e449324f556cd2564b232ee07acfb
Bhaanavee C S <151053362+Bhaanavee@users.noreply.github.com>
2025-07-21 19:29:53 +0530

    configured specific layers within the loaded Caffe model

--- p1.py
+++ p1.py
@@ -15,3 +15,16 @@ if not os.path.exists(MODEL):
 
 
 ap=argparse.ArgumentParser()
+ap.add_argument("-i", "--image", required=True, help="Path to input image")
+args=vars(ap.parse_args())
+
+print("Load model")
+net=cv2.dnn.readNetFromCaffe(PROTOTXT,MODEL)
+pts=np.load(POINTS)
+
+class8=net.getLayerId("class8_ab")
+conv8=net.getLayerId("conv8_313_rh")
+pts=pts.transpose().reshape(2,313,1,1)
+net.getLayer(class8).blobs=[pts.astype("float32")]
+net.getLayer(conv8).blobs=[np.full([1,313],2.606,dtype="float32")]
+
